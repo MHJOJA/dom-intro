e4564850a9521021363c89a8a08c4c4c6cd2af9d
MHJOJA <dmenzi694@gmail.com>
2020-06-24 19:15:36 +0200

    initial commit

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,10 +1,39 @@
 // get a reference to the sms or call radio buttons
+const billItemType = document.querySelector(".billItemTypeRadio");
 
 //get a reference to the add button
+const adBtnElement = document.querySelector(".radioBillAddBtn");
 
 //create a variable that will keep track of the total bill
+const TotalBill = document.querySelector("callTotalTwo");
 
 //add an event listener for when the add button is pressed
+
+
+var callsTotal = 0;
+var smsTotal = 0;
+
+function radioBillTotal(){
+       
+    var billTypeEntered = billItemType.value.trim();
+    
+    if (billTypeEntered === "call"){
+        callsTotal += 2.75
+    }
+    else if (billTypeEntered === "sms"){
+        smsTotal += 0.75;
+    }else {
+        billTotal = '';
+    }
+    
+    //update the totals that is displayed on the screen.
+    callsTotalElem.innerHTML = callsTotal.toFixed(2);
+    smsTotalElem.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalCostElem.innerHTML = totalCost.toFixed(2);
+    styleTotal();
+}
+adBtnElement.addEventListener('click', radioBillTotal())
 
 //in the event listener get the value from the billItemTypeRadio radio buttons
 // * add the appropriate value to the running total
